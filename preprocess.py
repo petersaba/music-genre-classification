d76ff612f593dcb62508d8709096bd806a01016a
Peter Saba <petersaba001@gmail.com>
2022-10-19 13:28:41 +0300

    preprocessing: adding all genres to the data dictionary

--- preprocess.py
+++ preprocess.py
@@ -9,14 +9,25 @@ JSON_FILENAME = 'dataset.json'
 
 def save_mfccs(dataset_dir_path, json_path=JSON_FILENAME, sr=SAMPLE_RATE, hop_length=HOP_LENGTH, n_fft=N_FFT, nb_segments=NB_SEGMENTS):
 
+    data = {
+        "genres": [],
+        "mfcc": [],
+        "labels": []
+    }
+
     for i, (root, dir_array, file_array) in enumerate(os.walk(dataset_dir_path)):
         
         if root != dataset_dir_path:
-            for file in file_array:
-                signal, sr = librosa.load(f'./{root}/{file}', sr=SAMPLE_RATE)
-                mfcc = librosa.feature.mfcc(y=signal, hop_length=hop_length, n_mfcc=13, n_fft=n_fft)
-                print(f'{file} has been read')
+            # for file in file_array:
+            #     signal, sr = librosa.load(f'./{root}/{file}', sr=SAMPLE_RATE)
+            #     mfcc = librosa.feature.mfcc(y=signal, hop_length=hop_length, n_mfcc=13, n_fft=n_fft)
+            #     print(f'{file} has been read')
+            pass
+        else:
+            for dir in dir_array:
+                data['genres'].append(dir)
 
+    print(data)
 
 if __name__ == '__main__':
     os.chdir('dataset')
