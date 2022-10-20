e4306d1230953791e3c91c28d3c85d1ed57293e3
Peter Saba <petersaba001@gmail.com>
2022-10-20 21:10:04 +0300

    preprocessing: making sure that the added mfccs have the expected nb of vectors

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import os
 import librosa
 import json
+import math
 
 SAMPLE_RATE = 22050
 SAMPLES_PER_TRACK = 22050 * 30
@@ -18,24 +19,33 @@ def save_mfccs(dataset_dir_path, json_path=JSON_FILENAME, sr=SAMPLE_RATE, hop_le
     }
 
     samples_per_segment = int(SAMPLES_PER_TRACK / nb_segments)
+    expected_nb_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
 
     for i, (root, dir_array, file_array) in enumerate(os.walk(dataset_dir_path)):
         
         if root != dataset_dir_path:
-            i -= 1 # first value of i is for the root directory which cannot be used to label the audio genres
+            # first value of i is for the root directory which cannot be used to label the audio genres
+            i -= 1 
+
             for file in file_array:
-                    signal, sr = librosa.load(f'./{root}/{file}', sr=SAMPLE_RATE)
+                signal, sr = librosa.load(f'./{root}/{file}', sr=SAMPLE_RATE)
 
-                    for current_segment in range(nb_segments):
-                        segment_start = samples_per_segment * current_segment
-                        segment_end = samples_per_segment * (current_segment + 1)
+                for current_segment in range(nb_segments):
+                    segment_start = samples_per_segment * current_segment
+                    segment_end = samples_per_segment * (current_segment + 1)
 
-                        segment_samples = signal[segment_start:segment_end]
-                        mfcc = librosa.feature.mfcc(y=segment_samples, hop_length=hop_length, n_mfcc=13, n_fft=n_fft)
-                        mfcc = mfcc.T
+                    segment_samples = signal[segment_start:segment_end]
+                    mfcc = librosa.feature.mfcc(y=segment_samples, hop_length=hop_length, n_mfcc=13, n_fft=n_fft)
+                    
+                    # mfcc needs to be transposed to be in the right format
+                    mfcc = mfcc.T
+
+                    # make sure that all saved mfccs have the same number of vectors so that the data is uniform
+                    if len(mfcc) == expected_nb_mfcc_vectors_per_segment:
                         data['mfcc'].append(mfcc.tolist())
                         data['labels'].append(i)
-                    print(f'{file} has been read')
+                print(f'{file} has been read')
+                
         else:
             for dir in dir_array:
                 data['genres'].append(dir)
