ee753504ceaff590d736beb4a9a0499932d16897
Peter Saba <petersaba001@gmail.com>
2022-10-20 23:08:21 +0300

    genre classification: building network architecture

--- genre_classifier.py
+++ genre_classifier.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from sklearn.model_selection import train_test_split
+import tensorflow.python.keras as keras
 
 DATASET_PATH = './dataset/dataset.json'
 
@@ -17,5 +18,18 @@ def load_dataset(dataset_path):
 if __name__ == '__main__':
 
     inputs, targets = load_dataset(DATASET_PATH)
-
     inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs, targets, test_size=0.3)
+
+    # Sequential model: moves from the left to the right 
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
+
+        # relu is generally better fir training models than sigmoid and most importantly
+        # because it avoids the vanishing gradient problem as derivative of sigmoid func is 0.25 at most
+        keras.layers.Dense(512, activation='relu'),
+        keras.layers.Dense(256, activation='relu'),
+        keras.layers.Dense(64, activation='relu'),
+
+        # nb of neurons is the same as the number of options in this case 10 genres
+        keras.layers.Dense(10, activation='softmax')
+    ])
