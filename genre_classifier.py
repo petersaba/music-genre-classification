45ea1920f72b9db0742283165fd2ca734c94520b
Peter Saba <petersaba001@gmail.com>
2022-10-20 22:50:45 +0300

    genre classifier: splitting dataset into training and test sets

--- genre_classifier.py
+++ genre_classifier.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 DATASET_PATH = './dataset/dataset.json'
 
@@ -16,3 +17,5 @@ def load_dataset(dataset_path):
 if __name__ == '__main__':
 
     inputs, targets = load_dataset(DATASET_PATH)
+
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs, targets, test_size=0.3)
