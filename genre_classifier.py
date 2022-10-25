e071f30ed8a50f9141c0d4b7d863b35dc0050bc6
Peter Saba <petersaba001@gmail.com>
2022-10-26 01:23:35 +0300

    genre classification: solving overfitting problem

--- genre_classifier.py
+++ genre_classifier.py
@@ -2,7 +2,8 @@ import json
 from pickletools import optimize
 import numpy as np
 from sklearn.model_selection import train_test_split
-import tensorflow.python.keras as keras
+from tensorflow import keras
+import matplotlib.pyplot as plt
 
 DATASET_PATH = './dataset/dataset.json'
 
@@ -24,19 +25,22 @@ if __name__ == '__main__':
     # Sequential model: moves from the left to the right 
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
+        keras.layers.Dropout(0.37),
 
-        # relu is generally better fir training models than sigmoid and most importantly
+        # relu is generally better for training models than sigmoid and most importantly
         # because it avoids the vanishing gradient problem as derivative of sigmoid func is 0.25 at most
-        keras.layers.Dense(512, activation='relu'),
-        keras.layers.Dense(256, activation='relu'),
-        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(512, activation='relu', kernel_regularizer=keras.regularizers.l2(0.005)),
+        keras.layers.Dropout(0.37),
+
+        keras.layers.Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l2(0.005)),
+        keras.layers.Dropout(0.37),
+
+        keras.layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.005)),
 
         # nb of neurons is the same as the number of options in this case 10 genres
         keras.layers.Dense(10, activation='softmax')
     ])
 
-    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+    optimizer = keras.optimizers.Adam(learning_rate=0.00015)
     model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.summary()
-
-    model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test), epochs=50, batch_size=32)
