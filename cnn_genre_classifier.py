0af47b16e4a9d76f93fd84b063665d4ae5806229
Peter Saba <petersaba001@gmail.com>
2022-10-27 14:05:59 +0300

    genre classifier with CNN: creating nn architecture

--- cnn_genre_classifier.py
+++ cnn_genre_classifier.py
@@ -1,8 +1,8 @@
 from json import load
 from genre_classifier import DATASET_PATH, load_dataset
-# from genre_classifier import load_dataset
 from sklearn.model_selection import train_test_split
 import numpy as np
+import tensorflow as tf
 
 def prepare_data(test_size, validation_size, dataset_path=DATASET_PATH):
 
@@ -18,5 +18,34 @@ def prepare_data(test_size, validation_size, dataset_path=DATASET_PATH):
 
     return X_train, X_test, X_validate, y_train, y_test, y_validate
 
+def create_model(input_shape):
+
+    keras = tf.keras
+    model = keras.Sequential()
+
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    model.add(keras.layers.Conv2D(32, (2, 2), activation='relu', input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(keras.layers.BatchNormalization())
+
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dropout(0.3))
+
+    model.add(keras.layers.Dense(10, activation='softmax'))
+
+    return model
+
 if __name__ == "__main__":
     X_train, X_test, X_validate, y_train, y_test, y_validate = prepare_data(0.25, 0.2)
+
+    # print(type(X_train))
+    input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
+    model = create_model(input_shape)
